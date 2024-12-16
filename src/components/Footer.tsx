ab86ea586c2463a33ee8c8a21d0cebcbd41de3dc
mfaizanakram8 <mfaizanakram536@gmail.com>
2024-12-17 00:41:53 +0500

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,70 +2,58 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
+
 const Footer = () => {
   return (
-    <div className="bg-gray-50 py-8 text-center">
+    <div className="bg-gray-50  py-8 text-center">
       {/* Logo */}
       <div className="mb-4 items-center justify-center text-center flex">
-        <Image src="/c.png" alt="logo" width={250} height={250} />
+     <Image src="/c.png" alt="logo" width={250} height={250}/>
       </div>
 
       {/* Navigation Links */}
       <div className="mb-6 space-x-4 text-sm">
-        <Link href="#">Home</Link>
+        <Link href="#" >
+          Home
+        </Link>
         <span>|</span>
-        <Link href="#">About</Link>
+        <Link href="#" >
+          About
+        </Link>
         <span>|</span>
-        <Link href="#">Features</Link>
+        <Link href="#" >
+          Features
+        </Link>
         <span>|</span>
-        <Link href="#">Portfolio</Link>
+        <Link href="#" >
+          Portfolio
+        </Link>
         <span>|</span>
-        <Link href="#">FAQs</Link>
+        <Link href="#">
+          FAQs
+        </Link>
         <span>|</span>
-        <Link href="#">Contact</Link>
+        <Link href="#" >
+          Contact
+        </Link>
       </div>
 
       {/* Social Media Links */}
       <div className="flex justify-center space-x-4">
         <Link href="#">
-          <Image
-            src="https://img.icons8.com/color/50/facebook-new.png"
-            alt="Facebook"
-            width={50}
-            height={50}
-          />
+        <img width="50" height="50" src="https://img.icons8.com/color/50/facebook-new.png" alt="facebook-new"/>
         </Link>
         <Link href="#">
-          <Image
-            src="https://img.icons8.com/fluency/48/google-logo.png"
-            alt="Google"
-            width={48}
-            height={48}
-          />
+        <img width="48" height="48" src="https://img.icons8.com/fluency/48/google-logo.png" alt="google-logo"/>
         </Link>
         <Link href="#">
-          <Image
-            src="https://img.icons8.com/color/50/pinterest--v1.png"
-            alt="Pinterest"
-            width={50}
-            height={50}
-          />
+        <img width="50" height="50" src="https://img.icons8.com/color/50/pinterest--v1.png" alt="pinterest--v1"/>
         </Link>
         <Link href="#">
-          <Image
-            src="https://img.icons8.com/color/50/twitter.png"
-            alt="Twitter"
-            width={50}
-            height={50}
-          />
+        <img width="50" height="50" src="https://img.icons8.com/color/50/twitter.png" alt="twitter"/>
         </Link>
         <Link href="#">
-          <Image
-            src="https://img.icons8.com/color/50/linkedin.png"
-            alt="LinkedIn"
-            width={50}
-            height={50}
-          />
+        <img width="50" height="50" src="https://img.icons8.com/color/50/linkedin.png" alt="linkedin"/>
         </Link>
       </div>
 
