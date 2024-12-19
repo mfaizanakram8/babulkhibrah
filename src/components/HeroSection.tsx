2e4871f3770fdc05bae0581378e64188fad80da3
mfaizanakram8 <mfaizanakram536@gmail.com>
2024-12-19 14:39:07 +0500

    update

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -123,7 +123,7 @@ export default function HeroSlider() {
       </div>
 
       {/* Cards Section */}
-      <div className="mt-32 py-12 px-6 sm:px-8 md:px-20 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6">
+      <div className="mt-32 py-12 px-6 sm:px-8 md:px-20 grid grid-cols-1 sm:grid-cols-2 min-[641px]:grid-cols-3  gap-6">
         {cards.map((card) => (
           <div
             key={card.id}
